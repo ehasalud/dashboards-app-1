6b906243a38944acf9c0410ba6a8fd0545133fc2
GitHub <noreply@github.com>
2019-04-24 08:02:47 +0000

    6569200 fix(filters): filter the list of dimensions usable as filters (#287)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.55c20056e1b5fe8ca8e2481fc0d1af83.js"
+  "./precache-manifest.52702f44c8a9aea7d8653aff10027309.js"
 );
 
 workbox.clientsClaim();
