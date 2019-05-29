31ca8a10b8ae91d84abd57742aad4212f6934d81
Edoardo Sabadelli <edoardo@dhis2.org>
2019-05-29 13:39:49 +0000

    c294622 Merge branch 'master' into biweekly-support

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0472b090ecd609fce93c5b507f81aef8.js"
+  "./precache-manifest.77a32883717ff788365ace894a3748a7.js"
 );
 
 workbox.clientsClaim();
