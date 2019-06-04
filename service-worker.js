831e3c32171ebd3786aec82a33719f9007042f56
GitHub <noreply@github.com>
2019-06-04 09:28:30 +0000

    09d4c87 fix: conditionally show the Show More button in item selector (#326)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.77a32883717ff788365ace894a3748a7.js"
+  "./precache-manifest.b3c03f6f38a5475c792431faf3c78ed5.js"
 );
 
 workbox.clientsClaim();
