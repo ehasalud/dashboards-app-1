0739a85df6e7af6611b6031a8f920fb2ae866e5f
GitHub <noreply@github.com>
2019-12-12 12:02:46 +0000

    d63499e fix: epi curve, update analytics and plugin deps (#521)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c87fde26a9cfda41905898c9fedfce72.js"
+  "./precache-manifest.48651c1ee1cb80a3066529de36623ee6.js"
 );
 
 workbox.clientsClaim();
