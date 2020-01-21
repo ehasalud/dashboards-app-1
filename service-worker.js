6df6c6dbbbd6012d1668caac12aba14acb67a596
GitHub <noreply@github.com>
2020-01-21 10:43:00 +0000

    3647034 fix: dimensions as array (DHIS2-7787) (#526)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e6f9af76a942ce5a81d193d6fe910dd0.js"
+  "./precache-manifest.6e64e19cf92e62f819599337a2168672.js"
 );
 
 workbox.clientsClaim();
