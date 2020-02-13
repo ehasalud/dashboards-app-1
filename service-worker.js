abb090b65b39bd3c45fdcc1ebe573fd8c89d30da
Jan Henrik Overland <janhenrik.overland@gmail.com>
2020-02-13 17:04:55 +0000

    05490f1 fix: @dhis2/data-visualizer-plugin@32.0.9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3e38ccdeacc0d2ee675fcfbc23ff3a17.js"
+  "./precache-manifest.c93434b294eb3cc42b0846588f0d867c.js"
 );
 
 workbox.clientsClaim();
