2de0279758f13d6f08c79b1b906ce3f7796caca3
GitHub <noreply@github.com>
2020-02-21 09:22:07 +0000

    1ccbd66 fix: upgrade data-visualizer-plugin to 34.2.5 (#566)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.269014a2802b9d78ddd6cf105ccd8700.js"
+  "./precache-manifest.842f2354055928b9bcc982bfd3474032.js"
 );
 
 workbox.clientsClaim();
