fbbc1ce035fae2d6bf3f4df6857eeb952c40a614
Jen Jones Arnesen <jennifer@dhis2.org>
2020-02-25 20:33:41 +0000

    827e8a5 fix: upgrade analytics to ^4.1.4

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b9acf149a500d25b49ee9825f08fa1d8.js"
+  "./precache-manifest.663456b9cde194a9546e6da210cf8e49.js"
 );
 
 workbox.clientsClaim();
