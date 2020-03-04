c23f0a3efc212d2a74528d76d1a2a47ec4b99abf
GitHub <noreply@github.com>
2020-03-04 08:55:16 +0000

    a5703c7 Merge branch 'master' into fix/plugin-loading-spinner-wip2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.133f808f900082da806933e278cabc92.js"
+  "./precache-manifest.44b7a8be70276203c65aaa573d378213.js"
 );
 
 workbox.clientsClaim();
