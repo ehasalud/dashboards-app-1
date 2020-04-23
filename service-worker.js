5c1c50031c4bc051c3b556aa60660aaf54a210e5
GitHub <noreply@github.com>
2020-04-23 19:51:56 +0000

    fcef0a5 chore: replace mui in delete dialog (#726)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d8fb4f27549b37abb37f1e4068e9b2c6.js"
+  "./precache-manifest.94e3b3f06776ac3b03dfd20c4c377910.js"
 );
 
 workbox.clientsClaim();
