98bdad6ecbaa260785cebcf0fbf06677d8db949a
GitHub <noreply@github.com>
2020-05-19 16:04:09 +0000

    d441366 chore: dedupe (TECH-381) (#846)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d421d9760ead12f7119edd764758c395.js"
+  "./precache-manifest.8ea146f52ea3e2d262f1503b330d5b56.js"
 );
 
 workbox.clientsClaim();
