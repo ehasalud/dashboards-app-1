e6015ffb05af7ffdbe617c976ebd622eb71dbaa5
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-05-19 08:48:00 +0000

    15c8d38 chore: deduplicate d2-i18n

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e1c57e9ef976283a57c3342413832769.js"
+  "./precache-manifest.d3b3d0081096c744a85e0cdf2472334a.js"
 );
 
 workbox.clientsClaim();
