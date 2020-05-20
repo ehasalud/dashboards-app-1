e14057a96f71b01bda9060c08829cc7f1088b0c5
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-05-20 09:27:17 +0000

    b7907d0 Merge remote-tracking branch 'origin/master' into fix/headerbar-translations

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ee0403d099842250e950a30757095939.js"
+  "./precache-manifest.83eda19f090db065b523bba28cd3a0b2.js"
 );
 
 workbox.clientsClaim();
