b7cefa214fb01fc6a7f0b361d04d4765723b6663
GitHub <noreply@github.com>
2020-05-22 08:48:00 +0000

    962b381 chore(deps): bump @dhis2/d2-ui-translation-dialog from 6.5.7 to 7.0.3

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4145f3d5d652bfdfe130399589720ff9.js"
+  "./precache-manifest.94aac8ee11bab892e6adf157d6a9cd03.js"
 );
 
 workbox.clientsClaim();
