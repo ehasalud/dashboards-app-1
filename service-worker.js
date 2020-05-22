6840cb1ac0f5a9dc3d0f571caec25d2321a48af3
GitHub <noreply@github.com>
2020-05-22 08:37:35 +0000

    fca9db8 chore(deps): bump @dhis2/d2-ui-interpretations from 6.5.7 to 7.0.3

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a61eab63835c52f7a3fc73c8628926b1.js"
+  "./precache-manifest.c58ee0b6ce8d11cdad01110decd1e611.js"
 );
 
 workbox.clientsClaim();
