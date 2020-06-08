9afff3edd25d6efb94fbdadf4d071c15f40c258d
GitHub <noreply@github.com>
2020-06-08 08:56:01 +0000

    e68bb50 chore(deps): bump @dhis2/data-visualizer-plugin from 35.0.4 to 35.0.5

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b78b4c6e5ecd9bd09378235a85f0ea7c.js"
+  "./precache-manifest.2ca1bbf10625c3a350cf4f5f385c67cf.js"
 );
 
 workbox.clientsClaim();
